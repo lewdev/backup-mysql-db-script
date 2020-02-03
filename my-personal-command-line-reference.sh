f921ee330820ec064ae63c6b5a13ab11e0cdf380
Lewis Nakao <lewdev@gmail.com>
2020-02-02 16:03:31 -1000

    Initial commit for commandline reference

---
+++ my-personal-command-line-reference.sh
@@ -0,0 +1,13 @@
+
+# my-personal-command-line-reference
+
+# Check mail on server.
+tail --lines=100 /var/spool/mail/root
+# or
+cat /var/spool/mail/root
+
+# clear mail
+> /var/spool/mail/root
+
+# check date/time on server
+date
